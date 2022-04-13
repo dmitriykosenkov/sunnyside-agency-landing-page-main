d7b06d362acd92f235a5c2bc04695667c782969d
dmitriykosenkov <dmkos23011991@gmail.com>
2022-04-14 00:09:36 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const burger = document.querySelector(".header__burger")
+const menu = document.querySelector(".header__menu")
+burger.addEventListener('click', () => {
+   menu.classList.toggle("open")
+})
